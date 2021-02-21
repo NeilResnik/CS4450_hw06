c3942373148db4b466d82cc9dfa3ca925994e5ad
Neil Resnik <neilr22@comcast.net>
2021-02-21 15:43:19 -0500

    Got my front end working with addison's backend

--- assets/js/Bulls.js
+++ assets/js/Bulls.js
@@ -1,116 +1,47 @@
-import "../css/app.css"
-
+import 'bulma/css/bulma.css'
+import '../css/app.scss'
 import React, { useState, useEffect } from 'react';
+import { GuessInput, GuessList } from './game'
+import { ch_join } from './socket'
 
-import { ch_join, ch_guess, ch_reset } from './socket';
-import GuessRow from './GuessRow';
-
-// WHAT STAYS HERE
-
-// guess history is no longer always 8 long
-//  - need to make a reverse then pad function that pads it with nulls and reverses
-
-export default function Bulls() {
-
-  // CUR GUESS IS VAL OF INPUT FORM AND USED FOR INPUT VALIDATION
-  const [curGuess, setCurGuess] = useState('');
-  const [state, setState] = useState({
-    guessHistory: [],
-    gameState: "playing",
-  })
-
-
-
-  let gameMessage = (<h2 className="endMsg"></h2>);
-
-  if (state.gameState === "won") {
-    gameMessage = (<h2 className="endMsg">YOU WON!</h2>)
-  }
-
-  if (state.gameState === "lost") {
-    gameMessage = (<h2 className="endMsg">You lost :(</h2>)
-  }
-
-  
-  useEffect(() => {
-    ch_join(setState);
-  });
-
-  function guess() {
-    // prevent invalid lengths
-    if (curGuess.length !== 4) {
-      return
-    }
-
-    ch_guess(curGuess.split(""))
-    
-    setCurGuess('');
-  }
-
-  function reset() {
-    ch_reset();
-    setCurGuess('');
-  }
-
-
-  // checks if key pressed was enter, calls guess() if it was
-  function handleKeyPress(ev) {
-    if (ev.key === 'Enter') {
-      guess()
-    }
-  }
-
-  // filter out duplicate characters, filter out non-string chars.
-  // HANDLES FORCING NO DUPLICATE NUMBERS
-  function handleValueChange(ev) {
-    // filter duplicates
-    let guessSet = new Set(ev.target.value);
-    let uniqueArr = [...guessSet];
-    // filter out NaNs
-    let result = uniqueArr.filter(c => {
-      return !isNaN(c);
+function Bulls() {
+    const [gameState, setGameState] = useState({
+        guesses: [],
+        results: [],
     });
 
-    // set the curGuess (input value) to the filterd string 
-    setCurGuess(result.join(''));
-  }
+    useEffect(() => {
+        ch_join(setGameState);
+    });
 
+    let notificationClassName = "notification";
+    if(gameState.results[0] === "4A0B") {
+        notificationClassName += " is-primary";
+    } else if(gameState.guesses.length >= 8){
+        notificationClassName += " is-danger";
+    } else {
+        notificationClassName += " is-hidden";
+    }
 
-  return (
-    <>
-      <div className="MainContainer">
-        <div className="HistoryContainer">
-          <div className="HistoryRow">
-            <div className="Red Number Cell">
-              #
-            </div>
-            <div className="Red Cell">
-              Guess
-            </div>
-            <div className="Red Cell">
-              Bulls
-            </div>
-            <div className="Red Cell">
-              Cows
-            </div>
-          </div>
-          {
-            state.guessHistory.map((guess, index) => (
-              <GuessRow key={index} index={index} guess={guess}/>
-            ))
-          }
+    return (
+        <div className="Bulls">
+           <section className="section">
+               <div className="container has-text-centered">
+                    <div className={notificationClassName}>
+                        {gameState.results[0] === "4A0B" ? "You Won!" : "You Lost!"}
+                    </div>
+                   <GuessInput enabled={gameState.results[0] !== "4A0B" && gameState.guesses.length < 8}/>
+                    <p className="is-size-3">Lives: {8 - gameState.guesses.length}</p>
+               </div>
+           </section>
+           <section className="section">
+               <div className="container has-text-centered">
+                   <h1 className="title is-size-2">Guesses:</h1>
+                   <GuessList guesses={gameState.guesses} results={gameState.results} />
+               </div>
+           </section>
         </div>
-        <div className="GuessContainer">
-          <input className="InputGuess" type="text" value={curGuess} onKeyDown={handleKeyPress} onChange={handleValueChange} maxLength="4"/>
-          <button className="GuessButton" onClick={guess}>Guess</button>
-          <button className="ResetButton" onClick={reset}>Reset</button>
-
-        </div>
-        <div>
-          {gameMessage}
-        </div>
-      </div>
-      <a className="Link" href="http://swoogity.com">Back to Home</a>
-    </>
-  );
+    );
 }
+
+export default Bulls;
---
+++ assets/js/game.js
@@ -0,0 +1,75 @@
+import 'bulma/css/bulma.css'
+import '../css/app.scss'
+import React, { useState } from 'react';
+import { ch_push, ch_reset } from './socket';
+
+function guess(text){
+    if(text.length < 4) {
+        return;
+    }
+    ch_push(text.split(""));
+}
+
+export function GuessInput({enabled}) {
+    const [text, setText] = useState("");
+
+    function updateText(event) {
+        let checked = "";
+        for (let c of event.target.value) {
+            if (checked.length < 4 && !checked.includes(c)
+                && c >= '0' && c <= '9') {
+                checked += c;
+            }
+        }
+        setText(checked);
+    }
+
+    function onKeypress(event) {
+        if(event.key === "Enter" && text.length === 4){
+            guess(text);
+        }
+    }
+
+    return (
+        <div className="field has-addons has-addons-centered">
+            <div className="control">
+                <input className="input"
+                    type="text" 
+                    placeholder="Guess"
+                    onChange={updateText}
+                    onKeyPress={onKeypress}
+                    disabled={!enabled}
+                    value={text}/>
+            </div>
+            <div className="control">
+                <button className="button is-primary"
+                        disabled={!enabled || text.length !== 4}
+                        onClick={() => { guess(text); }}>
+                    Guess 
+                </button>
+            </div>
+            <div className="control">
+                <button className="button is-danger"
+                    onClick={() => ch_reset({})}>
+                    Reset
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export function GuessList({guesses, results}) {
+    return(
+        <div className="content">
+            <ul className="no-marker">
+                {guesses.map(function(e, i){
+                    return (
+                        <li key={i}>
+                            <p>{e}: {results[i]}</p>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
