7ee97a5ef1a16167fdfb3fbf8680314242402756
Neil Resnik <neilr22@comcast.net>
2021-02-26 21:26:56 -0500

    prepare to deploy

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# TODO: Enable this script by removing the above.
-
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4780
+export PORT=4792
 
 echo "Stopping old copy of app, if any..."
 
